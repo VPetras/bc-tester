3a26052e9c092d2bc024aae2f088c293502f6565
VPetras <vojtechpetrasek@gmail.com>
2020-02-07 20:21:26 +0100

    add test led on rpi GPIO

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install python3-pip -y
+sudo apt install python-setuptools python-pygame python-opengl python-gst0.10 python-enchant gstreamer0.10-plugins-good python-dev build-essential libgl1-mesa-dev libgles2-mesa-dev zlib1g-dev -y
 pip3 install RPi.GPIO kivy
